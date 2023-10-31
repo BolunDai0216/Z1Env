905973d6378ebf4690f4ecc4c34827ee5b2218d1
PranayG <pren_gupta@outlook.com>
2023-10-31 15:02:03 -0400

    Update init

--- Z1Env/__init__.py
+++ Z1Env/__init__.py
@@ -9,3 +9,4 @@ def getDataPath():
 from Z1Env.differential_ik import DiffIK
 from Z1Env.gripper_env import GripperSim
 from Z1Env.z1_env import Z1Sim
+from Z1Env.boundary_detection import BoundaryDetection
